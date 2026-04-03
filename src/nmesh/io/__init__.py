df2d0b19301c006898dd6ad81a46fbdeaaab45fe
Emanuel Pituch <32016786+epituch@users.noreply.github.com>
2026-04-03 16:52:46 -0700

    Remove __all__

--- src/nmesh/io/__init__.py
+++ src/nmesh/io/__init__.py
@@ -2,9 +2,3 @@
 
 from .legacy_nmesh_hdf5 import load_raw_mesh_from_legacy_nmesh_hdf5
 from .meshio_support import load_raw_mesh_with_meshio, save_raw_mesh_with_meshio
-
-__all__ = [
-    "load_raw_mesh_from_legacy_nmesh_hdf5",
-    "load_raw_mesh_with_meshio",
-    "save_raw_mesh_with_meshio",
-]
